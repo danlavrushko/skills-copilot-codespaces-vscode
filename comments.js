d0ff755e4155021d2c9afb21e6779ba889fd515d
Dan Lavrushko <danylo.lavr@gmail.com>
2024-04-26 13:52:51 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create web server
+
+const express = require('express');
+
+// A web server with express.js
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
